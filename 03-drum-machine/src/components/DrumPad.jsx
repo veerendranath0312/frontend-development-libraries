efdc1879e39f8df75ca182a6bf05ff4dd9959031
Veerendranath <veerendranath0312@gmail.com>
2024-10-15 18:21:29 -0400

    Complete use case 6

--- 03-drum-machine/src/components/DrumPad.jsx
+++ 03-drum-machine/src/components/DrumPad.jsx
@@ -1,6 +1,11 @@
 function DrumPad({ drum, handleClick }) {
   return (
-    <div className="drum-pad" id={drum.normalId} onClick={handleClick}>
+    <div
+      className="drum-pad"
+      id={drum.normalId}
+      name={drum.key}
+      onClick={handleClick}
+    >
       {drum.key}
       <audio src={drum.normalSound} className="clip" id={drum.key}></audio>
     </div>
--- 03-drum-machine/src/components/DrumPads.jsx
+++ 03-drum-machine/src/components/DrumPads.jsx
@@ -1,18 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import drumData from '../drumData'
 import DrumPad from './DrumPad'
 
 function DrumPads() {
-  const handleKeyStroke = (e) => {
-    const keys = drumData.map((drum) => drum.key)
-    if (keys.includes(e.key.toUpperCase())) {
-      console.log('Pressed Key: ', e.key)
-    }
-  }
+  const drumPadsRef = useRef(null)
 
   useEffect(() => {
+    const handleKeyStroke = (e) => {
+      const keys = drumData.map((drum) => drum.key)
+      console.log(drumPadsRef.current)
+      if (keys.includes(e.key.toUpperCase())) {
+        // The namedItem() method of the HTMLCollection interface returns the
+        // first Element in the collection whose id or name attribute match
+        // the specified name, or null if no element matches.
+
+        // 'drumPadsRef.current.children' returns a HTML Collection
+        const drumPadEl = drumPadsRef.current.children.namedItem(
+          e.key.toUpperCase()
+        )
+
+        const audioEl = drumPadEl.children[0]
+        audioEl.currentTime = 0
+        audioEl.play()
+      }
+    }
+
     window.addEventListener('keydown', handleKeyStroke)
-    document.querySelector()
 
     return () => {
       window.addEventListener('keydown', handleKeyStroke)
@@ -26,7 +39,7 @@ function DrumPads() {
   }
 
   return (
-    <div className="drum-pads">
+    <div className="drum-pads" ref={drumPadsRef}>
       {drumData.map((drum) => (
         <DrumPad key={drum.key} drum={drum} handleClick={handleClick} />
       ))}
